5292dab0220e112c5e59fecf203586c33f8fb860
ClwnYeti <crazyyeti@mail.ru>
2021-11-17 02:18:04 +0300

    fix: some troubles in backup job

--- Backups/Services/BackupJob.cs
+++ Backups/Services/BackupJob.cs
@@ -8,29 +8,32 @@ namespace Backups.Services
 {
     public class BackupJob : IBackupJob
     {
+        private const string DirectoryForSave = "C:\\Users\\crazy\\RiderProjects\\ClwnYeti\\Backups\\Storages";
         private readonly List<JobObject> _currentJobObjects;
         private readonly List<RestorePoint> _restorePoints;
         private IArchiver _archiver;
-        private string _directory = "C:\\Users\\crazy\\RiderProjects\\ClwnYeti\\Backups\\Storages";
-        public BackupJob(IArchiver algorithmOfStorage, List<JobObject> jobObjects)
+        private IStorageRepository _repository;
+
+        public BackupJob(IArchiver algorithmOfStorage, List<JobObject> jobObjects, IStorageRepository repository)
         {
             Id = Guid.NewGuid();
             _archiver = algorithmOfStorage;
             _restorePoints = new List<RestorePoint>();
             _currentJobObjects = jobObjects;
+            _repository = repository;
         }
 
         public Guid Id { get; }
 
         public RestorePoint MakeRestorePoint()
         {
-            _restorePoints.Add(new RestorePoint(_restorePoints.Count, _archiver.Store(_directory, _currentJobObjects)));
+            _restorePoints.Add(new RestorePoint(_restorePoints.Count, _archiver.Store(_repository.Empty(), DirectoryForSave, _currentJobObjects)));
             return _restorePoints[^1];
         }
 
         public RestorePoint MakeRestorePoint(DateTime dateTime)
         {
-            _restorePoints.Add(new RestorePoint(_restorePoints.Count, _archiver.Store(_directory, _currentJobObjects), dateTime));
+            _restorePoints.Add(new RestorePoint(_restorePoints.Count, _archiver.Store(_repository.Empty(), DirectoryForSave, _currentJobObjects), dateTime));
             return _restorePoints[^1];
         }
 
