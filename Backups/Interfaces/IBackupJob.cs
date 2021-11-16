41527c414125411cc46fe91df77f2f8440032095
ClwnYeti <crazyyeti@mail.ru>
2021-11-17 01:45:32 +0300

    feat: add logic of backup job

--- Backups/Interfaces/IBackupJob.cs
+++ Backups/Interfaces/IBackupJob.cs
@@ -1,19 +1,15 @@
 using System;
-using System.Collections.Generic;
 using Backups.Classes;
 
 namespace Backups.Interfaces
 {
     public interface IBackupJob
     {
-        public JobObject CreateJobObject(string fileName);
-        public void ChangeJobObject(string fileName);
-        public void ChangeJobObject(JobObject jobObject);
-        public void DeleteJobObject(string fileName);
-        public void DeleteJobObject(JobObject jobObject);
         public RestorePoint MakeRestorePoint();
         public RestorePoint MakeRestorePoint(DateTime dateTime);
-        public List<RestorePoint> GetRestorePointsThatCreatedAfterDate(DateTime dateTime);
-        public List<RestorePoint> GetLastCreatedNumOfRestorePoints(int num);
+
+        public JobObject Add(string pathToFile);
+
+        public void Delete(string pathToFile);
     }
 }
--- Backups/Services/BackupJob.cs
+++ Backups/Services/BackupJob.cs
@@ -1,30 +1,51 @@
+using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using Backups.Classes;
 using Backups.Interfaces;
 
 namespace Backups.Services
 {
-    public class BackupJob
+    public class BackupJob : IBackupJob
     {
         private readonly List<JobObject> _currentJobObjects;
         private readonly List<RestorePoint> _restorePoints;
         private IArchiver _archiver;
-        public BackupJob(IArchiver algorithmOfStorage)
+        private string _directory = "C:\\Users\\crazy\\RiderProjects\\ClwnYeti\\Backups\\Storages";
+        public BackupJob(IArchiver algorithmOfStorage, List<JobObject> jobObjects)
         {
+            Id = Guid.NewGuid();
             _archiver = algorithmOfStorage;
             _restorePoints = new List<RestorePoint>();
-            _currentJobObjects = new List<JobObject>();
+            _currentJobObjects = jobObjects;
         }
 
-        public JobObject CreateJobObject(FileStream fileStream)
+        public Guid Id { get; }
+
+        public RestorePoint MakeRestorePoint()
         {
-            _currentJobObjects.Add(new JobObject(fileStream.Name));
+            _restorePoints.Add(new RestorePoint(_restorePoints.Count, _archiver.Store(_directory, _currentJobObjects)));
+            return _restorePoints[^1];
+        }
+
+        public RestorePoint MakeRestorePoint(DateTime dateTime)
+        {
+            _restorePoints.Add(new RestorePoint(_restorePoints.Count, _archiver.Store(_directory, _currentJobObjects), dateTime));
+            return _restorePoints[^1];
+        }
+
+        public JobObject Add(string pathToFile)
+        {
+            _currentJobObjects.Add(new JobObject(pathToFile));
             return _currentJobObjects[^1];
         }
 
-        public void ChangeJobObject(FileStream fileStream)
+        public void Delete(string pathToFile)
         {
+            foreach (JobObject j in _currentJobObjects.Where(j => j.Path == pathToFile))
+            {
+                _currentJobObjects.Remove(j);
+            }
         }
     }
 }
