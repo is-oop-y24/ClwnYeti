c3a9c2e378adf50835edecb99d7a7ae227efbbc1
ClwnYeti <crazyyeti@mail.ru>
2021-11-17 17:17:28 +0300

    feat: add tests for file system

--- Backups/Program.cs
+++ Backups/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.IO;
+using Backups.Classes;
+using Backups.Interfaces;
+using Backups.Services;
 
 namespace Backups
 {
@@ -6,6 +9,26 @@ namespace Backups
     {
         private static void Main()
         {
+            FileStream firstFile = File.Create("C:\\Users\\crazy\\RiderProjects\\ClwnYeti\\Backups\\JobObjects\\1.txt");
+            firstFile.Close();
+            FileStream secondFile = File.Create("C:\\Users\\crazy\\RiderProjects\\ClwnYeti\\Backups\\JobObjects\\2.txt");
+            secondFile.Close();
+            FileStream thirdFile = File.Create("C:\\Users\\crazy\\RiderProjects\\ClwnYeti\\Backups\\JobObjects\\3.txt");
+            thirdFile.Close();
+            FileStream fourthFile = File.Create("C:\\Users\\crazy\\RiderProjects\\ClwnYeti\\Backups\\JobObjects\\4.txt");
+            fourthFile.Close();
+            var firstTest = new BackupJob(new SplitStoragesArchiver(), new StorageRepositoryWithFileSystem(), "C:\\Users\\crazy\\RiderProjects\\ClwnYeti\\Backups\\Backups");
+            var secondTest = new BackupJob(new SingleStorageArchiver(), new StorageRepositoryWithFileSystem(), "C:\\Users\\crazy\\RiderProjects\\ClwnYeti\\Backups\\Backups");
+            firstTest.Add(firstFile.Name);
+            firstTest.Add(secondFile.Name);
+            firstTest.MakeRestorePoint();
+            firstTest.Delete(firstFile.Name);
+            firstTest.MakeRestorePoint();
+            secondTest.Add(thirdFile.Name);
+            secondTest.Add(fourthFile.Name);
+            secondTest.MakeRestorePoint();
+            secondTest.Add(firstFile.Name);
+            secondTest.MakeRestorePoint();
         }
     }
 }
