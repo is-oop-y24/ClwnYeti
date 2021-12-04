41898077d340c0548cc32499afc64a796e2ad0be
ClwnYeti <crazyyeti@mail.ru>
2021-12-04 13:42:42 +0300

    fix: conversations

--- Banks/Classes/BankConfiguration.cs
+++ Banks/Classes/BankConfiguration.cs
@@ -1,18 +1,44 @@
+using Banks.Tools;
+
 namespace Banks.Classes
 {
     public class BankConfiguration
     {
+        private DebitAccountConfiguration _debitAccountConfiguration;
+        private DepositAccountConfiguration _depositAccountConfiguration;
+        private CreditAccountConfiguration _creditAccountConfiguration;
+
         public BankConfiguration(DebitAccountConfiguration debitAccountConfiguration, DepositAccountConfiguration interestsForDepositAccount, CreditAccountConfiguration creditAccountConfiguration, decimal criticalAmountOfMoney)
         {
+            if (debitAccountConfiguration == null || interestsForDepositAccount == null || creditAccountConfiguration == null)
+            {
+                throw new BankException("Unexpected null reference");
+            }
+
             DebitAccountConfiguration = debitAccountConfiguration;
             DepositAccountConfiguration = interestsForDepositAccount;
             CreditAccountConfiguration = creditAccountConfiguration;
             CriticalAmountOfMoney = criticalAmountOfMoney;
         }
 
-        public DebitAccountConfiguration DebitAccountConfiguration { get; set; }
-        public DepositAccountConfiguration DepositAccountConfiguration { get; set; }
-        public CreditAccountConfiguration CreditAccountConfiguration { get; set; }
+        public DebitAccountConfiguration DebitAccountConfiguration
+        {
+            get => _debitAccountConfiguration;
+            set => _debitAccountConfiguration = value ?? throw new BankException("Unexpected null reference");
+        }
+
+        public DepositAccountConfiguration DepositAccountConfiguration
+        {
+            get => _depositAccountConfiguration;
+            set => _depositAccountConfiguration = value ?? throw new BankException("Unexpected null reference");
+        }
+
+        public CreditAccountConfiguration CreditAccountConfiguration
+        {
+            get => _creditAccountConfiguration;
+            set => _creditAccountConfiguration = value ?? throw new BankException("Unexpected null reference");
+        }
+
         public decimal CriticalAmountOfMoney { get; }
 
         public static BankConfiguration Default()
